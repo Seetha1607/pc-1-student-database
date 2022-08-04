743ec6a8baaa5650a5a3fa3729fd94a0771298f5
bhakti.rathod <bhakti.rathod@stackroute.in>
2022-08-04 11:58:23 +0530

    Initial commit

---
+++ src/main/java/com/jap/StudentDemo.java
@@ -0,0 +1,20 @@
+package com.jap;
+
+public class StudentDemo
+{
+
+    public void getAllStudentDetails(){
+      //Load the Drivers
+        // Get the connection from database
+
+        //Create statement object
+
+        //execute the query
+
+        //display the ResultSet Data
+    }
+    public static void main( String[] args ) {
+        StudentDemo studentDemo = new StudentDemo();
+        studentDemo.getAllStudentDetails();
+    }
+}
