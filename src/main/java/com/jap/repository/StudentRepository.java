c8e91446752495d059bacebefe4685c8e393c166
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-16 18:10:51 +0530

    created getAllStudentDetails() method in StudentRepository

--- src/main/java/com/jap/repository/StudentRepository.java
+++ src/main/java/com/jap/repository/StudentRepository.java
@@ -7,5 +7,21 @@
 
 package com.jap.repository;
 
+import com.jap.model.Student;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class StudentRepository {
+    public List<Student> getAllStudentDetails(Connection databaseConnection) {
+        //Load the Drivers
+        // Get the connection from database
+
+        //Create statement object
+
+        //execute the query
+
+        //display the ResultSet Data
+        return List.of();
+    }
 }
