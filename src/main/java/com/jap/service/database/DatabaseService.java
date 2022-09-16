63c43c34bdee90049815a3284c3e3bf08e24e6dc
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-16 17:53:50 +0530

    created fields of DatabaseService

--- src/main/java/com/jap/service/database/DatabaseService.java
+++ src/main/java/com/jap/service/database/DatabaseService.java
@@ -8,4 +8,8 @@
 package com.jap.service.database;
 
 public class DatabaseService {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/school";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "admin";
 }
